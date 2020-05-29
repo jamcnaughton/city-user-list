686fc9e2b35e5844d983fb18f651dad9add4a3d8
JAMcNaughton <james.andrew.mcnaughton@gmail.com>
2020-05-29 16:39:25 +0100

    Improved proxy usage.

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -4,7 +4,7 @@
 export const appConfig = {
   apiUrl: 'https://bpdts-test-app.herokuapp.com',
   milesFromTargetCity: 50,
-  proxy: 'https://cors-anywhere.herokuapp.com/',
+  proxy: '', // If you have CORS issues try 'https://cors-anywhere.herokuapp.com/',
   targetCity: 'London',
   targetCityLatitude: '51.509865',
   targetCityLongitude: '-0.118092',
--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import './styles.scss';
 import userService from './services/user.service';
 import { appConfig } from './config/app-config';
 
+/**
+ * The user-list element to manipulate.
+ */
+let userList;
+
+/**
+ * The spinner element to manipulate.
+ */
+let spinner;
+
 // Wait for DOM content to load.
 document.addEventListener(
   'DOMContentLoaded',
   () => {
+
+    // Get the elements to manipulate.
+    userList = document.getElementById('user-list');
+    spinner = document.getElementById('spinner');
 
     // Add a listener for the city users buttons.
     const cityUsersButton = document.getElementById('city-users');
@@ -22,15 +36,17 @@ document.addEventListener(
  * Make a request to the API for getting users in the supplied city.
  */
 function getCityUsers () {
-
+  
   // Make request to the API for city users.
+  showSpinner(true);
   userService.httpGetCityUsers(
     appConfig.targetCity,
     (users) => {
+      showSpinner(false);
       if (users) {
-        // TODO Display users on page.
+        displayUsers(users);
       } else {
-        // TODO Display error to user.
+        displayError('Unable to retrieve users in the city');
       }
     }
   );
@@ -42,25 +58,75 @@ function getCityUsers () {
  */
 function getNearbyUsers () {
 
-  // Establish arrays of users from requests.
-
   // Make request to the API.
+  showSpinner(true);
   userService.getNearbyUsers(
     appConfig.targetCity,
     appConfig.targetCityLatitude,
     appConfig.targetCityLongitude,
     appConfig.milesFromTargetCity,
     (users) => {
+      showSpinner(false);
       if (users) {
-        // TODO Display users on page.
+        displayUsers(users);
       } else {
-        // TODO Display error to user.
+        displayError('Unable to retrieve users nearby to the city');
       }
     }
   );
 
 }
 
-// TODO Function for populating the user list.
+/**
+ * Display the list of users in the main component.
+ *
+ * @param {{}[]} users An array of users
+ */
+function displayUsers (users) {
 
-// TODO Function for showing an error.
+  // Clear user-list.
+  clearUserList();
+
+  // TODO Display the list of users in the user list (handle empty lists too).
+
+}
+
+/**
+ * Show an error message in the main component.
+ *
+ * @param {string} message The message to show the user.
+ */
+function displayError (message) {
+
+  // Clear user-list.
+  clearUserList();
+
+  // Display an error message in the user list.
+  const errorDiv = document.createElement('div');
+  errorDiv.className = 'error-message';
+  errorDiv.innerHTML = message;
+  userList.appendChild(errorDiv);
+
+}
+
+/**
+ * Clear the contents of the user list.
+ */
+function clearUserList () {
+  for (const child of userList.children) {
+    userList.removeChild(child);
+  }
+}
+
+/**
+ * Control the visibility of the spinner.
+ *
+ * @param {boolean} show Flag determining whether to show the spinner (if true) or hide it.
+ */
+function showSpinner (show) {
+  if (show) {
+    spinner.classList.remove('hidden');
+  } else {
+    spinner.classList.add('hidden');
+  }
+}
