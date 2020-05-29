afdf6933dd8479fadafa28c1085c14c6ca25ca6b
JAMcNaughton <james.andrew.mcnaughton@gmail.com>
2020-05-29 15:26:28 +0100

    Added better method of working out distances.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,28 @@ import './styles.scss';
 import userService from './services/user.service';
 import { appConfig } from './config/app-config';
 
-/**
- * Make a request to the API for getting users in London.
- */
-function getLondonUsers () {
+// Wait for DOM content to load.
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
 
-  // Make request to the API.
+    // Add a listener for the city users buttons.
+    const cityUsersButton = document.getElementById('city-users');
+    cityUsersButton.addEventListener('click', getCityUsers);
+
+    // Add a listener for the nearby users buttons.
+    const nearbyUsersButton = document.getElementById('nearby-users');
+    nearbyUsersButton.addEventListener('click', getNearbyUsers);
+
+  }
+);
+
+/**
+ * Make a request to the API for getting users in the supplied city.
+ */
+function getCityUsers () {
+
+  // Make request to the API for city users.
   userService.httpGetCityUsers(
     appConfig.targetCity,
     (users) => {
@@ -19,6 +35,32 @@ function getLondonUsers () {
     }
   );
 
-  // TODO Add a function for getting users outside London but within 50 miles.
+}
+
+/**
+ * Make requests to the API for getting users nearby the supplied city.
+ */
+function getNearbyUsers () {
+
+  // Establish arrays of users from requests.
+
+  // Make request to the API.
+  userService.getNearbyUsers(
+    appConfig.targetCity,
+    appConfig.targetCityLatitude,
+    appConfig.targetCityLongitude,
+    appConfig.milesFromTargetCity,
+    (users) => {
+      if (users) {
+        // TODO Display users on page.
+      } else {
+        // TODO Display error to user.
+      }
+    }
+  );
 
 }
+
+// TODO Function for populating the user list.
+
+// TODO Function for showing an error.
