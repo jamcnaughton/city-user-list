344eee45721d4b66c40e891230dd3fea55a60071
JAMcNaughton <james.andrew.mcnaughton@gmail.com>
2020-06-01 10:28:05 +0100

    Added display of returned users to main component.

--- src/index.js
+++ src/index.js
@@ -23,7 +23,17 @@ document.addEventListener(
 
     // Add a listener for the nearby users buttons.
     const nearbyUsersButton = document.getElementById('nearby-users');
-    nearbyUsersButton.addEventListener('click', getNearbyUsers);
+    nearbyUsersButton.addEventListener(
+      'click',
+      ()=> {
+
+        // Disable the button then call the method for getting the nearby users.
+        nearbyUsersButton.disabled = true;
+        nearbyUsersButton.setAttribute('aria-disabled', true);
+        getNearbyUsers();
+
+      }
+    );
 
   }
 );
@@ -70,8 +80,34 @@ function displayUsers (users) {
   // Clear user-list.
   clearUserList();
 
-  // TODO Display the list of users in the user list (handle empty lists too).
-  console.log(users);
+  // Loop through users.
+  for (const user of users) {
+
+    // Create a div to display user information in.
+    const userDiv = document.createElement('div');
+    userDiv.className = 'user-entry';
+
+    // Create a header to display the user name in.
+    const userHeader = document.createElement('h2');
+    userHeader.className = 'user-header';
+    const userFirstName = user['first_name'] ? user['first_name'] : '?';
+    const userLastName = user['last_name'] ? user['last_name'] : '?';
+    userHeader.innerHTML = `${userFirstName} ${userLastName}`;
+
+    // Create a span to display the user e-mail in.
+    const userSpan = document.createElement('h2');
+    userSpan.className = 'user-span';
+    const userEmail = user['email'] ? user['email'] : '?';
+    userSpan.innerHTML = userEmail;
+
+    // Attach the header and the span to the div.
+    userDiv.appendChild(userHeader);
+    userDiv.appendChild(userSpan);
+
+    // Attach the div to the user-list div.
+    userList.appendChild(userDiv);
+
+  }
 
 }
 
